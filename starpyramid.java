ea070f1a0c54a7a7f0c2c139ce9a45f33ff9f11e
Jenith073 <jenithsundaran@gmail.com>
2025-09-11 11:15:16 +0530

    Create starpyramid.java

---
+++ starpyramid.java
@@ -0,0 +1,15 @@
+public class starpyramid {
+    public static void main(String[] args) {
+     for (int i=1;i<=6;i++){
+        for(int s=i;s<=6;s++){
+            System.out.print(" ");
+        }
+            for (int j=1;j<=1*i;j++){
+                System.out.print("* ");
+            }
+        
+        System.out.println();
+     }   
+    }
+}
+
